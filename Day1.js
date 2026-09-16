9de698a4e230036c5eda2a87672dc9865fa3a396
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-16 13:42:17 +0530

    Day 1 of learning JavaScript

---
+++ Day1.js
@@ -0,0 +1,19 @@
+let age=10;
+console.log(age);
+age=20;
+console.log(age);
+
+
+//String
+
+let Doublename="Satyajit"
+console.log(Doublename);
+let singlename='satyajit'
+console.log(singlename);
+
+let developer = "Jessica";
+developer[0] = "M";
+console.log(developer);
+
+developer="Satyajit"
+console.log(developer);
