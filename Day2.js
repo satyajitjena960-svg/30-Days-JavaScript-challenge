780a4e653d62ccec343cfcf4174ab281a21cec50
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-17 19:43:41 +0530

    Day 2 of learning JavaScript

--- Day2.js
+++ Day2.js
@@ -164,3 +164,31 @@ const lowercaseWord = "camelcase";
 const camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase()+lowercaseWord.slice(-3);
 console.log("Camel cased version:");
 console.log(camelCasedVersion);
+
+//practice 4
+
+const originalString = "I love cats.";
+console.log("Original string:");
+console.log(originalString);
+
+const replacedString = originalString.replace("cats", "dogs");
+console.log("After using the replace() method:");
+console.log(replacedString);
+
+const exampleSentence = "I love cats and cats are so much fun!";
+console.log("Original sentence:");
+console.log(exampleSentence);
+
+const dogsOnlySentence = exampleSentence.replaceAll("cats", "dogs");
+console.log("Replacing all occurrences of cats with dogs:");
+console.log(dogsOnlySentence);
+
+const learningSentence = "I love learning!";
+console.log("Original learning sentence:");
+console.log(learningSentence);
+
+const repeatedLove = "love ".repeat(3).trimEnd();
+console.log(repeatedLove);
+
+const newSentence = `I ${repeatedLove} learning.`;
+console.log(newSentence);
