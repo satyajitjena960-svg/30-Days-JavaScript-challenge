420995ea2dd18313b7fd09d0b03cb48f25f9004b
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-17 22:17:33 +0530

    Day 2 of learning JavaScript

--- Day2.js
+++ Day2.js
@@ -262,3 +262,80 @@ if (timmyAge >= 16) {
 } else {
   console.log("Timmy is not old enough to drive.");
 }
+//PRACTICE 6
+
+let fortune1="Your cat will look very cuddly today.";
+let fortune2="The weather will be nice tomorrow.";
+let fortune3="Be cautious of your new neighbors.";
+let fortune4="You will find a new hobby soon.";
+let fortune5="It would be wise to avoid the color red today.";
+
+let randomNumber=Math.floor(Math.random() * 5) + 1 
+
+let selectedFortune ;
+
+if(randomNumber==1){
+  selectedFortune=fortune1;
+}else if(randomNumber==2){
+  selectedFortune=fortune2;
+}else if(randomNumber==3){
+  selectedFortune=fortune3;
+}else if(randomNumber==4){
+  selectedFortune=fortune4;
+}else {
+  selectedFortune=fortune5;
+}
+console.log(selectedFortune )
+
+//PRACTICE 7
+
+const botName = "MathBot";
+const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
+
+console.log(greeting);
+
+console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
+
+const randomNum = Math.random();
+console.log(randomNum);
+
+console.log("Now, generate a random number between two values.");
+
+const min = 1;
+const max = 100;
+
+const randomNum2 = Math.random() * (max - min) + min;
+console.log(randomNum2);
+
+console.log("The Math.floor() method rounds the value down to the nearest whole integer.");
+
+const numRoundedDown = Math.floor(6.7);
+console.log(numRoundedDown);
+
+console.log("Now, generate a random integer between two values.");
+
+const randomInt = Math.floor(Math.random() * (max - min) + min);
+console.log(randomInt);
+
+console.log("The Math.ceil() method rounds the value up to the nearest whole integer.");
+
+const numRoundedUp = Math.ceil(3.2);
+console.log(numRoundedUp);
+
+console.log(
+  "The Math.round() method rounds the value to the nearest whole integer."
+);
+
+const numRounded = Math.round(2.7);
+console.log(numRounded);
+const numRounded2 = Math.round(11.2);
+console.log(numRounded2);
+
+console.log("The Math.max() and Math.min() methods are used to get the maximum and minimum number from a range.");
+
+const maxNum = Math.max(3, 125, 55, 24);
+console.log(maxNum);
+const minNum = Math.min(6, 90, 14, 90, 2);
+console.log(minNum);
+
+console.log("It was fun learning about the different Math methods with you!")
