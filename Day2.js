1c1deaddbfd9ee267ae4e3a8fb690f506f77950b
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-17 19:26:56 +0530

    Day 2 of learning JavaScript

--- Day2.js
+++ Day2.js
@@ -132,3 +132,35 @@ const endPunctuation = message.slice(-1);
 console.log(`The ending punctuation mark is a "${endPunctuation}"`);
 
 console.log("Workshop complete! You now know how to use includes() and slice().")
+
+
+//practice 3
+
+const userInput = "   Hello World!   ";
+console.log("Original input:");
+console.log(userInput);
+
+const cleanedInput = userInput.trim();
+console.log("Result of trimming whitespace from both ends:");
+console.log(cleanedInput);
+
+const trimmedStart = userInput.trimStart();
+console.log("After using the trimStart() method, leading spaces removed:");
+console.log(trimmedStart);
+
+const trimmedEnd = userInput.trimEnd();
+console.log("After using the trimEnd() method, trailing spaces removed:");
+console.log(trimmedEnd);
+
+const upperCaseInput = cleanedInput.toUpperCase();
+console.log("Result of using the toUpperCase() method:");
+console.log(upperCaseInput);
+
+const lowerCaseInput = cleanedInput.toLowerCase();
+console.log("Result of using the toLowerCase() method:");
+console.log(lowerCaseInput);
+
+const lowercaseWord = "camelcase";
+const camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase()+lowercaseWord.slice(-3);
+console.log("Camel cased version:");
+console.log(camelCasedVersion);
