22276a5001877dc7c0eb6f497e15d644cfd2b50b
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-18 14:53:20 +0530

    Day 3 of learning JavaScript

--- Day3.js
+++ Day3.js
@@ -110,3 +110,25 @@ function convertCtoF(a){
   return (a * 9/5) + 32
 
 }
+
+
+
+// building a card counting assistant
+let count=0;
+
+
+function cardCounter(card){
+  if(card==2 || card==3|| card==4 || card==5 || card==6){
+    count++;
+  }
+  if(card==10||card=="J"||card=="Q"||card=="K"||card=="A"){
+    count--;
+  }
+  
+  if(count>0){
+  return count+" "+"Bet";
+}else
+   return count+" "+"Hold";
+}
+
+
