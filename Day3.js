347ef988182a0523e784bf7fe7819d37586b9b4e
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-18 15:05:56 +0530

    Day 3 of learning JavaScript

--- Day3.js
+++ Day3.js
@@ -132,3 +132,14 @@ function cardCounter(card){
 }
 
 
+//leap year
+function isLeapYear (num){
+if(num%4==0&&num%100!=0 || num%400==0){
+  return `${num} is a leap year.`
+}
+return `${num} is not a leap year.`
+}
+
+let year=2004;
+const result=isLeapYear(year);
+console.log(result);
