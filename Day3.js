bed0189e8db6f63fe066510ffe4954bfecc7a070
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-18 14:24:08 +0530

    Day 3 of learning JavaScript

--- Day3.js
+++ Day3.js
@@ -67,3 +67,38 @@ console.log(maskEmail("apple.pie@example.com"))
 console.log(maskEmail("freecodecamp@example.com"))
 console.log(maskEmail("info@test.dev"))
 console.log(maskEmail("user@domain.org"))
+
+
+
+//function with if else
+
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+function getLoanMessage(annualIncome, creditScore) {
+  if(creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
+    return "You qualify for a duplex, condo, and car loan.";
+  } else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+    return "You qualify for a condo and car loan.";
+  } else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
+    return "You qualify for a car loan.";
+  } else {
+    return "You don't qualify for any loans.";
+  }
+}
+
+const duplexLoanMsg = getLoanMessage(85000, 850);
+const condoLoanMsg= getLoanMessage(65000, 690);
+const carLoanMsg= getLoanMessage(45000, 660);
+const noLoanMsg= getLoanMessage(25000, 550);
+
+console.log(duplexLoanMsg)
+console.log(condoLoanMsg)
+console.log(carLoanMsg)
+console.log(noLoanMsg)
