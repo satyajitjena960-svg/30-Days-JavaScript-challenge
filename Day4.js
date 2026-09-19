112e96f82a2c5c0d7e93b5fab45aee9d43e479d3
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-19 20:12:39 +0530

    Day 4 of learning JavaScript

--- Day4.js
+++ Day4.js
@@ -40,3 +40,55 @@ shoppingList.shift();
 shoppingList[0] = "Canola Oil";
 
 console.log(getShoppingListMsg(shoppingList));
+
+
+
+// Lunch picker PRogram
+
+const lunches =[]
+function addLunchToEnd (arr,string){
+arr.push(string)
+console.log(`${string} added to the end of the lunch menu.`);
+return arr;
+}
+function addLunchToStart (arr,string){
+  arr.unshift(string)
+  console.log(`${string} added to the start of the lunch menu.`)
+  return arr;
+}
+function removeLastLunch  (arr){
+  if(arr.length<=0){
+    console.log("No lunches to remove.")
+  }else{
+     let element=arr.pop();
+console.log(`${element} removed from the end of the lunch menu.`);
+}
+return arr;
+}
+function removeFirstLunch   (arr){
+  if(arr.length<=0){
+    console.log("No lunches to remove.")
+  }else{
+     let element=arr.shift();
+console.log(`${element} removed from the start of the lunch menu.`);
+}
+return arr;
+}
+
+function getRandomLunch (arr){
+  if(arr.length<=0){
+    console.log("No lunches available." )
+  }else{
+      const randomIndex = Math.floor(Math.random() * arr.length);
+
+console.log(`Randomly selected lunch: ${arr[randomIndex]}`);
+}
+}
+function showLunchMenu (arr){
+  if(arr.length<=0){
+    console.log("The menu is empty.")
+  }else{
+     console.log(`Menu items: ${arr.join(", ")}`)
+  }
+
+}
