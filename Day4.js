5a754fc089feacf90c30acd785b810c5b61913a7
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-19 22:09:28 +0530

    Day 4 of learning JavaScript

--- Day4.js
+++ Day4.js
@@ -117,3 +117,66 @@ function golfScore(num1,num2){
     return "Go Home!"
   }
 }
+
+
+
+
+//Wild life tracker
+
+const tiger = {
+  species: "Tiger",
+  age: 5,
+  isEndangered: true
+};
+
+const elephant = {
+  species: "Elephant",
+  age: 10,
+  isEndangered: true
+};
+
+const getSpecies = (animal) => {
+  return animal.species;
+};
+
+console.log(getSpecies(tiger));
+
+const getAge = (animal) => {
+  return animal.age;
+};
+
+console.log(getAge(tiger));
+
+const addHabitat = (animal, habitat) => {
+  animal.habitat = habitat;
+  return animal;
+};
+
+console.log(addHabitat(tiger, "Rainforest"));
+
+const updateAge = (animal, newAge) => {
+  animal.age = newAge;
+  return animal;
+};
+
+console.log(updateAge(elephant, 12));
+
+const removeEndangeredStatus = (animal) => {
+  delete animal.isEndangered;
+  return animal;
+};
+
+console.log(removeEndangeredStatus(tiger));
+
+const hasHabitat = (animal) => {
+  return animal.hasOwnProperty("habitat");
+};
+
+console.log(hasHabitat(tiger));
+console.log(hasHabitat(elephant));
+
+const getProperty = (animal , propertyName) => animal[propertyName]
+
+
+console.log(getProperty(tiger, "species"))
+console.log(getProperty(elephant, "age"))
