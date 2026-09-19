eb426d3e5dde507fa982f15f8731bd3bae3f19b2
satyajitjena960-svg <satyajitjena960@gmail.com>
2026-09-19 20:20:49 +0530

    Day 4 of learning JavaScript

--- Day4.js
+++ Day4.js
@@ -92,3 +92,28 @@ function showLunchMenu (arr){
   }
 
 }
+
+
+
+//golf score translator
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(num1,num2){
+  if(num2==1){
+    return "Hole-in-one!";
+  }else if(num2<=num1-2){
+    return "Eagle";
+  }
+  else if(num2==num1-1){
+    return "Birdie";
+  }else if(num2==num1){
+    return "Par";
+  }else if(num2==num1+1){
+    return "Bogey";
+  }else if(num2==num1+2){
+    return "Double Bogey";
+  }else if(num2>=num1+3){
+    return "Go Home!"
+  }
+}
